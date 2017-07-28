646a08b3777a71b491dd2552ca31393b6c884c0a
jesschen <jessica_j_chen@outlook.com>
2017-07-27 23:06:38 -0700

    got apicall working with basic status code (says "Florida population is 200"

--- src/apicall.js
+++ src/apicall.js
@@ -62,6 +62,51 @@ function httpsGet(myData, callback) {
 
     // Update these options with the details of the web service you would like to call
     var options = {
+        host: 'api.musixmatch.com',
+        port: 443,
+        path: '/ws/1.1/artist.search?q_artist=justin%20bieber&apikey=8b7654870c8395335a30eb19039218f6',
+        method: 'GET',
+
+        // if x509 certs are required:
+        // key: fs.readFileSync('certs/my-key.pem'),
+        // cert: fs.readFileSync('certs/my-cert.pem')
+    };
+
+    var req = https.request(options, res => {
+        res.setEncoding('utf8');
+        var returnData = "";
+
+        res.on('data', chunk => {
+            returnData = returnData + chunk;
+        });
+
+        res.on('end', () => {
+            // we have now received the raw return data in the returnData variable.
+            // We can see it in the log output via:
+            // console.log(JSON.stringify(returnData))
+            // we may need to parse through it to extract the needed data
+
+            var pop = JSON.parse(returnData).message.header.status_code;
+
+            callback(pop);  // this will execute whatever function the caller defined, with one argument
+
+        });
+
+    });
+    req.end();
+
+}
+
+
+function httpsGet2(myData, callback) {
+
+    // GET is a web service request that is fully defined by a URL string
+    // Try GET in your browser:
+    // https://cp6gckjt97.execute-api.us-east-1.amazonaws.com/prod/stateresource?usstate=New%20Jersey
+
+
+    // Update these options with the details of the web service you would like to call
+    var options = {
         host: 'cp6gckjt97.execute-api.us-east-1.amazonaws.com',
         port: 443,
         path: '/prod/stateresource?usstate=' + encodeURIComponent(myData),
